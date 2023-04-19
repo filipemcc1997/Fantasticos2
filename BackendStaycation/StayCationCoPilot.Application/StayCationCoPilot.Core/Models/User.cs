dacd12a5228fe946f1ab33b43d548471f81a6b59
fmccoelho-sonaemc <95624175+fmccoelho-sonaemc@users.noreply.github.com>
2023-04-19 15:12:14 +0100

    Change User.cs

--- BackendStaycation/StayCationCoPilot.Application/StayCationCoPilot.Core/Models/User.cs
+++ BackendStaycation/StayCationCoPilot.Application/StayCationCoPilot.Core/Models/User.cs
@@ -4,16 +4,11 @@
     {
         //create properties for User
         public int UserId { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public int Zip { get; set; }
-        public string Country { get; set; }
-        public string PaymentMethod { get; set; }
 
     }
 }
